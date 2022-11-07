3f193076fbf3903fb465cb7d0e3825a363ed0865
truongdz <you@example.com>
2022-11-07 18:17:44 +0700

    ajax

--- src/main/java/com/vn/controller/InjectionScheduleController.java
+++ src/main/java/com/vn/controller/InjectionScheduleController.java
@@ -34,18 +34,8 @@ public class InjectionScheduleController {
     @Autowired
     InjectionScheduleService injectionScheduleService;
 
-//    @GetMapping("/add/injectionSchedule")
-//    public String listInjection(Model model) {
-//        model.addAttribute("injection",new InjectionScheduleDTO());
-//        List<InjectionSchedule> scheduleList= injectionScheduleReponsitory.findAll();
-//        model.addAttribute("listInjection", scheduleList);
-//        List<Vaccine> vaccineList= vaccineReponsitory.findAll();
-//        model.addAttribute("listVaccine",vaccineList);
-//        return "create-injection-schedule";
-//    }
-
-    @GetMapping("/schedule/add/injection")
-    public String getAdd(Model model){
+    @GetMapping("/add/injectionSchedule")
+    public String listInjection(Model model) {
         model.addAttribute("injection",new InjectionScheduleDTO());
         List<InjectionSchedule> scheduleList= injectionScheduleReponsitory.findAll();
         model.addAttribute("listInjection", scheduleList);
@@ -53,6 +43,19 @@ public class InjectionScheduleController {
         model.addAttribute("listVaccine",vaccineList);
         return "create-injection-schedule";
     }
+    @GetMapping("/lisst")
+    public String getlist(){
+        return "injectionScheduleList";
+    }
+//    @GetMapping("/schedule/add/injection")
+//    public String getAdd(Model model){
+//        model.addAttribute("injection",new InjectionScheduleDTO());
+//        List<InjectionSchedule> scheduleList= injectionScheduleReponsitory.findAll();
+//        model.addAttribute("listInjection", scheduleList);
+//        List<Vaccine> vaccineList= vaccineReponsitory.findAll();
+//        model.addAttribute("listVaccine",vaccineList);
+//        return "create-injection-schedule";
+//    }
 
 //        @PostMapping("/add/injectionSchedule")
 //    public String addInjection(@Valid @ModelAttribute("injection") InjectionScheduleDTO injectionScheduleDTO, BindingResult bindingResult) {
