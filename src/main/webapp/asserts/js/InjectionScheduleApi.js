8d4f818db2b7b3e8e319816a70f767fd91094d1c
truongdz <you@example.com>
2022-11-09 10:52:03 +0700

    paging injection

--- src/main/webapp/asserts/js/InjectionScheduleApi.js
+++ src/main/webapp/asserts/js/InjectionScheduleApi.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+    Pageging();
+    create();
+ })
+
+
+function Pageging() {
     $.ajax({
-        url: 'http://localhost:8080/schedule/api/list',
+        url: 'http://localhost:8080/schedule/api/list?paging=',
         type: 'GET',
         dataType: 'JSON',
         success: function (res) {
             const vaccineNameApi = res.map(item => {
-                return "<tr><td><a href=\"\">" + item.vaccineName + "</a></td>\n" +
+                return "<tr><td><a href=\"#\">" + item.vaccineName + "</a></td>\n" +
                     "                                    <td>From " + item.startDate + " to " + item.endDate + "</td>\n" +
                     "                                    <td>" + item.place + "</td>\n" +
                     "                                    <td>" + item.note + "</td>\n" +
@@ -18,6 +24,58 @@ $(document).ready(function () {
             console.log(err);
         }
     })
+    var pageeeee=0;
+    var page=$('.page-item');
+    var curruentPage=1;
+    for (let i=0;i<page.length;i++){
+        page[i].onclick=function () {
+            var pageing=$(this).text();
+            curruentPage=pageing;
+            $.ajax({
+                url: 'http://localhost:8080/schedule/api/list?paging='+pageing,
+                type: 'GET',
+                dataType: 'JSON',
+                success: function (res) {
+                    const vaccineNameApi = res.map(item => {
+                        return "<tr><td><a href=\"\">" + item.vaccineName + "</a></td>\n" +
+                            "                                    <td>From " + item.startDate + " to " + item.endDate + "</td>\n" +
+                            "                                    <td>" + item.place + "</td>\n" +
+                            "                                    <td>" + item.note + "</td>\n" +
+                            "                                    <td>" + item.status + "</td>" +
+                            "</tr>"
+                    })
+                    $("#bodyList").html(vaccineNameApi);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            })
+        }
+    }
+    $(document).on("click",".page-item-next",function () {
+        curruentPage++;
+        $.ajax({
+            url: 'http://localhost:8080/schedule/api/list?paging='+curruentPage,
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (res) {
+                const vaccineNameApi = res.map(item => {
+                    return "<tr><td><a href=\"\">" + item.vaccineName + "</a></td>\n" +
+                        "                                    <td>From " + item.startDate + " to " + item.endDate + "</td>\n" +
+                        "                                    <td>" + item.place + "</td>\n" +
+                        "                                    <td>" + item.note + "</td>\n" +
+                        "                                    <td>" + item.status + "</td>" +
+                        "</tr>"
+                })
+                $("#bodyList").html(vaccineNameApi);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    })
+}
+function create() {
     $("#btnSave").on("click", function () {
         var vaccineId = $("#vaccineID").val();
         var startDate = $(".startDate").val();
@@ -43,4 +101,7 @@ $(document).ready(function () {
             }
         })
     })
-})
+}
+function search() {
+
+}
--- src/main/webapp/asserts/js/ajaxIndex.js
+++ src/main/webapp/asserts/js/ajaxIndex.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(document).on("click", "#listSchedule", function () {
         $.ajax({
             method: "GET",
-            url: "http://localhost:8080/schedule/api/list",
+            url: "http://localhost:8080/lisst",
             success: function (res) {
                 $("#mainIndex").html(res)
             },
