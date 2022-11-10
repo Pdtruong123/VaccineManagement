d61f40905e67052cf9b294c64eec98fb48620055
truongdz <you@example.com>
2022-11-10 17:50:50 +0700

    search

--- src/main/java/com/vn/model/InjectionSchedule.java
+++ src/main/java/com/vn/model/InjectionSchedule.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.*;
 import com.vn.util.GenerateID;
@@ -32,12 +33,13 @@ public class InjectionSchedule implements Serializable {
     })
     @Column(name = "injection_schedule_id", length = 36)
     private String id;
-
+    @NotBlank
     @Column(length = 1000)
     private String description;
+    @NotBlank
     @Column(name = "end_date")
     private LocalDate endDate;
-
+    @NotBlank
     private String place;
 
     public Vaccine getVaccine() {
@@ -47,7 +49,7 @@ public class InjectionSchedule implements Serializable {
     public void setVaccine(Vaccine vaccine) {
         this.vaccine = vaccine;
     }
-
+    @NotBlank
     @Column(name = "start_date")
     private LocalDate startDate;
 
