71464f5090cc8ec7f8085cbc43c116043436c837
Trung <trung@example.com>
2022-11-11 23:22:58 +0700

    add vaccintype

--- src/main/java/com/vn/model/Vaccine.java
+++ src/main/java/com/vn/model/Vaccine.java
@@ -13,6 +13,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.testng.annotations.AfterClass;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
