81358ad6964d1feaadd01b3074857b0034381fef
Ferb2608 <hieuct260898@gmail.com>
2022-11-14 23:35:13 +0700

    add table data

--- src/main/java/com/vn/controller/InjectionResultController.java
+++ src/main/java/com/vn/controller/InjectionResultController.java
@@ -6,21 +6,14 @@ import com.vn.service.InjectionResultService;
 import com.vn.service.VaccineService;
 import com.vn.util.DataInjectionResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class InjectionResultController {
