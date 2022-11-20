25116dbce86486a0f5afc0166ebc4b0ea387291e
Kien <phamtrungkien31101998@gmail.com>
2022-11-20 10:39:33 +0700

    Merge branch 'master' of bitbucket.org:trungvu1188/mockprojectgroup2 into customer
    # Conflicts:
    #	src/main/java/com/vn/model/Customer.java
    #	src/main/java/com/vn/model/InjectionSchedule.java
    #	src/main/resources/application.properties
    #	src/main/webapp/WEB-INF/views/common/tiles.xml
    #	src/main/webapp/WEB-INF/views/customer/registerCustomer.jsp
    #	src/main/webapp/asserts/js/DataTables.js

--- src/main/java/com/vn/FptMockProjectApplication.java
+++ src/main/java/com/vn/FptMockProjectApplication.java
@@ -1,9 +1,7 @@
 package com.vn;
 
-import com.vn.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class FptMockProjectApplication {
