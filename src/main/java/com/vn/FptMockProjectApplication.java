92bd3294db0779e7869ee5a1c8e742b7c6a419de
Kien <phamtrungkien31101998@gmail.com>
2022-11-21 18:43:35 +0700

    Merge branch 'master' of bitbucket.org:trungvu1188/mockprojectgroup2 into customer
    # Conflicts:
    #	src/main/java/com/vn/model/Customer.java
    #	src/main/java/com/vn/model/InjectionSchedule.java
    #	src/main/resources/application.properties
    #	src/main/webapp/WEB-INF/views/common/tiles.xml
    #	src/main/webapp/WEB-INF/views/customer/registerCustomer.jsp
    #	src/main/webapp/asserts/js/DataTables.js

--- src/main/java/com/vn/FptMockProjectApplication.java
+++ src/main/java/com/vn/FptMockProjectApplication.java
@@ -1,7 +1,9 @@
 package com.vn;
 
+import com.vn.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class FptMockProjectApplication {
--- src/main/java/com/vn/config/SecurityConfig.java
+++ src/main/java/com/vn/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setPasswordEncoder(passwordEncoder);
 		provider.setUserDetailsService(customUserDetailServiceImpl);
-		
+
 		auth.authenticationProvider(provider);
 	}
 
@@ -49,27 +49,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.cors().disable();
-		
-			
-			http.formLogin()
-			.loginPage("/login")
-			.loginProcessingUrl("/j_spring_security_check")
-            .usernameParameter("userName")
-            .passwordParameter("password")
-            .defaultSuccessUrl("/home")
-            .failureHandler(new AuthenticationFailureHandler() {
-				@Override
-				public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-						AuthenticationException exception) throws IOException, ServletException {
-					String userName = request.getParameter("userName");
-                    String error = exception.getMessage();
-                    System.out.println("A failed login attempt with userName: "
-                                        + userName + ". Reason: " + error);
- 
-                    String redirectUrl = request.getContextPath() + "/login?error";
-                    response.sendRedirect(redirectUrl);
-					
-				}
+
+
+		http.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/j_spring_security_check")
+				.usernameParameter("userName")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/home")
+				.failureHandler(new AuthenticationFailureHandler() {
+					@Override
+					public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+														AuthenticationException exception) throws IOException, ServletException {
+						String userName = request.getParameter("userName");
+						String error = exception.getMessage();
+						System.out.println("A failed login attempt with userName: "
+								+ userName + ". Reason: " + error);
+
+						String redirectUrl = request.getContextPath() + "/login?error";
+						response.sendRedirect(redirectUrl);
+
+					}
             })
             .and()
             .rememberMe()
@@ -77,21 +77,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .tokenValiditySeconds(2 * 24 * 60 * 60)
             .key("asdasdsadsadasdsad")
             .rememberMeCookieName("cookieRemember")
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .deleteCookies("cookieRemember", "JSESSIONID")
-            .clearAuthentication(true)
-            .and()
-			.authorizeHttpRequests()
-			.antMatchers("/register", "/login","/login?error","*.css","*.webp", "*.js","/","*.img","/css/**","/img/**","/js/**", "/asserts/**" ,"/fontawesome-free-6.1.2-web/**").permitAll()
-			.antMatchers("/home","/injection-result/list","/news/list").hasAnyAuthority("ADMIN","USER")
-            .antMatchers("/employee/**","/customer/**","/injection-schedule/**","/injection-result/**","/vaccine/**","/vaccineType/**","/news/**","/report/**").hasAnyAuthority("ADMIN")
-            
-            .anyRequest().authenticated();
-		
-	}
-	
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.deleteCookies("cookieRemember", "JSESSIONID")
+				.clearAuthentication(true)
+				.and()
+				.authorizeHttpRequests()
+				.antMatchers("/register", "/login", "/login?error", "*.css", "*.webp", "*.js", "/", "*.img", "/css/**", "/img/**", "/js/**", "/asserts/**", "/fontawesome-free-6.1.2-web/**").permitAll()
+				.antMatchers("/home", "/injection-result/list", "/news/list").hasAnyAuthority("ADMIN", "USER")
+				.antMatchers("/employee/**", "/customer/**", "/injection-schedule/**", "/injection-result/**", "/vaccine/**", "/vaccineType/**", "/news/**", "/report/**").hasAnyAuthority("ADMIN")
 
-	
+				.anyRequest().authenticated();
+
+	}
+
+
 }
